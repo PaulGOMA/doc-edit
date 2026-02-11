f828812a160992d7d77b8c8c09f2803ee7e61056
Paul Valerie GOMA <paulgoma07@gmail.com>
2026-02-11 09:53:56 +0100

    Adding the font family list

---
+++ src/utils/FontFamilyList.ts
@@ -0,0 +1,7 @@
+export const FONT_FAMILIES = [
+  { label: 'Arial', value: 'Arial, sans-serif' },
+  { label: 'Times New Roman', value: "'Times New Roman', serif" },
+  { label: 'Courier New', value: "'Courier New', monospace" },
+  { label: 'Roboto', value: 'Roboto, sans-serif' },
+  { label: 'Open Sans', value: "'Open Sans', sans-serif" },
+]
