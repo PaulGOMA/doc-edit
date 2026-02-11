3cfb4cb89e7605be4fb850230aae26c6f850f692
Paul Valerie GOMA <paulgoma07@gmail.com>
2026-02-11 09:41:08 +0100

    Adding the toolbar assistant

---
+++ src/ui/assistants/ToolBarAssistant.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="toolbar-assistant-container">
+    <slot></slot>
+  </div>
+</template>
+
+<style lang="css">
+.toolbar-assistant-container {
+  width: fit-content;
+  min-height: 2rem;
+  max-height: fit-content;
+  background: white;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border: 1.25px solid #e2e8f0;
+  border-radius: 5px;
+}
+</style>
