28782625169f51ae09236c6a50196a92ef368787
Paul Valerie GOMA <paulgoma07@gmail.com>
2026-02-13 08:53:31 +0100

    Update the widget factory

--- src/core/node_factory/WidgetFactory.ts
+++ src/core/node_factory/WidgetFactory.ts
@@ -1,6 +1,8 @@
 import { TextWidget } from '../widgets/TextWidget'
 import type { WidgetType, WidgetModel } from '../models/WidgetModel'
+import { ImageWidget } from '../widgets/ImageWidget'
 
-export const WidgetFactory: Record<WidgetType, () => WidgetModel> = {
+export const WidgetFactory: Record<WidgetType, (...args: any) => WidgetModel> = {
   text: () => new TextWidget().createNode(),
+  image: (src: string) => new ImageWidget(src).createNode(),
 }
